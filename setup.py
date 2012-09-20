35f9ffdf300e9d6a02633be92a6ffc7f9a7898fb
Jonathan Sick <jonathansick@mac.com>
2012-09-20 17:59:26 -0400

    Running setup.py docs will now build Sphinx docs

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@
 import os
 from setuptools import setup
 
+try:
+    from sphinx.setup_command import BuildDoc
+    cmdclass = {'docs': BuildDoc}
+except ImportError:
+    BuildDoc = None
+    cmdclass = {}
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -11,6 +18,7 @@ def read(fname):
 setup(
     name="ghostmap",
     packages=["ghostmap"],
+    cmdclass=cmdclass,
     author="Jonathan Sick",
     author_email="jonathansick@mac.com",
     description="Delaunay and Voronoi tessellation for astronomers.",

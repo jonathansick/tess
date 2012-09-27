d582f612f9477516dc927d475e41ad5dc1b67080
Jonathan Sick <jonathansick@mac.com>
2012-09-27 16:02:39 -0400

    setup.py can build ctypes extensions with a hack

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 import os
 from setuptools import setup
+from distutils import ccompiler
 
 try:
     from sphinx.setup_command import BuildDoc
@@ -13,6 +14,30 @@ except ImportError:
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+# If necessary, use include_dirs=[np.get_include()]) in Extension()
+# from https://gist.github.com/3313315
+def build_ctypes_ext(name, sources):
+    compiler = ccompiler.new_compiler()
+    compiler.compile(sources)
+
+    def get_onames(sources):
+        o_names = []
+        for source in sources:
+            head, tail = os.path.split(source)
+            o_name = os.path.splitext(tail)[0] + ".o"
+            o_names.append(os.path.join(head, o_name))
+        return o_names
+
+    compiler.link_shared_object(get_onames(sources), name + ".so")
+
+# Build the ctypes extensions into .so shared libraries
+# TODO include the build .so file in the installation
+# see http://packages.python.org/distribute/setuptools.html#including-data-files
+# Perhaps exhange the build_ctypes_ext for an autotools-based make setup
+# which may be more robust
+build_ctypes_ext("ghostmap/_lloyd", ["ghostmap/_lloyd.c"])
 
 
 setup(

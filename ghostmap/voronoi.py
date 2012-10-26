d312454c16b19bdffc2ac511747e25fbe915b336
Jonathan Sick <jonathansick@mac.com>
2012-10-26 13:06:21 -0400

    voronoi: refactored out setting the pixel grid context

--- ghostmap/voronoi.py
+++ ghostmap/voronoi.py
@@ -20,28 +20,54 @@ class VoronoiTessellation(object):
         self.yNode = y  #: Array of node y-coordinates
         self.segmap = None  #: 2D `ndarray` of `vBinNum` for each pixel
         self.cellAreas = None  #: 1D array of Voronoi cell areas
+        self.xlim = None  #: Length-2 array of min, max coords of x pixel grid
+        self.ylim = None  #: Length-2 array of min, max coords of y pixel grid
+        self.header = None  #: a PyFITS header representing pixel grid
 
-    def make_segmap(self, header=None, xlim=None, ylim=None):
+    def set_pixel_grid(self, xlim, ylim):
+        """Set a pixel grid bounding box for the tessellation. This is
+        used when rendering Voronoi fields or computing cell areas.
+
+        Setting the pixel grid is a prerequistie for running the methods:
+        - :meth:`make_segmap` and :meth:`save_segmap`
+        - :meth:`compute_cell_areas`
+
+        :param xlim, ylim: tuples of (min, max) pixel ranges.
+        """
+        assert len(xlim) == 2, "xlim must be (min, max) sequence"
+        assert len(ylim) == 2, "ylim must be (min, max) sequence"
+        self.xlim = xlim
+        self.ylim = ylim
+
+    def set_fits_grid(self, header):
+        """Convenience wrapper to :meth:`set_pixel_grid` if a FITS header is
+        available. As a bonus, the FITS header will be used when saving
+        any rendered fields to FITS.
+        
+        .. note:: The header is available as the :attr:`self.header` attribute.
+
+        :param header: a PyFITS image header, defines area of rendered Voronoi
+            images (e.g. segmentaion maps or fields).
+        """
+        # Assume origin at 1, FITS standard
+        xlim = (1, header['NAXIS2'] + 1)
+        ylim = (1, header['NAXIS1'] + 1)
+        self.set_pixel_grid(xlim, ylim)
+        self.header = header
+
+    def make_segmap(self):
         """Make a pixel segmentation map that paints the Voronoi bin number
         on Voronoi pixels.
         
         The result is stored as the `segmap` attribute and returned to the
         caller.
         
-        :param header: pyfits header, used to define size of segmentation map.
-        :param xlim, ylim: tuples of (min, max) pixel ranges, used if
-                           `header` is `None`.
         :returns: The segmentation map array, `segmap`.
         """
-        if header is not None:
-            # Assume origin at 1, FITS standard
-            xlim = (1, header['NAXIS2'] + 1)
-            ylim = (1, header['NAXIS1'] + 1)
-        else:
-            assert xlim is not None, "Need a xlim range (min, max)"
-            assert ylim is not None, "Need a ylim range (min, max)"
-        xgrid = np.arange(xlim[0], xlim[1])
-        ygrid = np.arange(ylim[0], ylim[1])
+        assert self.xlim is not None, "Need to run `set_pixel_grid()` first"
+        assert self.ylim is not None, "Need to run `set_pixel_grid()` first"
+        xgrid = np.arange(self.xlim[0], self.xlim[1])
+        ygrid = np.arange(self.ylim[0], self.ylim[1])
         # Package xNode and yNode into Nx2 array
         # y is first index if FITS data is also structured this way
         yxNode = np.hstack(self.yNode, self.xNode)
@@ -50,20 +76,19 @@ class VoronoiTessellation(object):
         self.segmap = griddata(yxNode, np.arange(0, self.yNode.shape[0]),
                 (xgrid, ygrid), method='nearest')
 
-    def save_segmap(self, fitsPath, **kwargs):
+    def save_segmap(self, fitsPath):
         """Convenience wrapper to :meth:`make_segmap` that saves the
         segmentation map to a FITS file.
 
         :param fitsPath: full filename destination of FITS file
-        :param kwargs: keyword arguments are passed to :meth:`make_segmap`.
         """
         import pyfits
         fitsDir = os.path.dirname(fitsPath)
         if not os.path.exists(fitsDir): os.makedirs(fitsDir)
         if self.segmap is None:
-            self.make_segmap(**kwargs)
-        if 'header' in kwargs:
-            pyfits.writeto(fitsPath, self.segmap, kwargs['header'])
+            self.make_segmap()
+        if self.header is not None:
+            pyfits.writeto(fitsPath, self.segmap, self.header)
         else:
             pyfits.writeto(fitsPath, self.segmap)
 
@@ -132,4 +157,8 @@ class VoronoiTessellation(object):
         canvas = FigureCanvas(fig)
         ax = fig.add_subplot(111)
         ax.plot(self.xNode, self.yNode, 'ok')
+        if self.xlim is not None:
+            ax.set_xlim(self.xlim)
+        if self.ylim is not None:
+            ax.set_ylim(self.ylim)
         canvas.print_figure(plotPath)

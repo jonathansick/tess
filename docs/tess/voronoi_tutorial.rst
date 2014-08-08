1e1d98517be1e6f754bce356383768100ddfca9b
Jonathan Sick <jonathansick@mac.com>
2014-08-08 12:46:40 -0600

    Update tutorial with new Voronoi api

--- docs/tess/voronoi_tutorial.rst
+++ docs/tess/voronoi_tutorial.rst
@@ -1,5 +1,5 @@
-Tutorial: Equal-S/N Pixel Binning with Voronoi Diagrams
-=======================================================
+Equal-S/N Pixel Binning with Centroidal Voronoi Tessellation
+============================================================
 
 :mod:`tess` can be used to bin images into equal S/N regions.
 This procedure was popularized by `Cappellari & Copin 2003 <http://adsabs.harvard.edu/abs/2003MNRAS.342..345C>`_ in the context of SAURON IFS imaging.
@@ -10,7 +10,7 @@ To begin, we'll make a mock data set with a 2D Gaussian and a fixed noise level:
    import numpy as np
    shape = (256, 256)
    img = 10. * np.exp(-((pix_x - 128.) ** 2. / (2. * 50. ** 2.) +
-                         (pix_y - 128.) ** 2. / (2. * 50. ** 2.)))
+                        (pix_y - 128.) ** 2. / (2. * 50. ** 2.)))
    noise = np.ones(shape, dtype=float)
    pix_sn = img / noise
 
@@ -31,24 +31,14 @@ Here's a map of those centroids and pixel segmentation into roughly equal S/N bi
 .. image:: images/iso_sn_accretion.*
 
 Of course, this is not a Voronoi map; the bins have odd shapes dictated by the circumstances of the pixel accretion.
-But we can use these generator centroids as seeds for a Centroidal Voronoi Tessellation (CVT), which convergently segments the pixels into a Voronoi segmentation so that the Voronoi nodes coincide with the centre of mass of each Voronoi cell::
+But we can use these generator centroids as seeds for a Centroidal Voronoi Tessellation (CVT), which segments the pixels into a Voronoi tessellation so that the Voronoi nodes coincide with the centre of mass of each Voronoi cell and each Voronoi cell has approximately equal mass (or in this case, S/N)::
 
-
-   pix_x, pix_y = np.meshgrid(np.arange(shape[1], dtype=float),
-                              np.arange(shape[0], dtype=float))
    from tess.cvtessellation import CVTessellation
-   cvt = CVTessellation(pix_x.flatten(),
-                        pix_y.flatten(),
-                        pix_sn.flatten() ** 2.,
-                        node_xy=generator_centroids)
-   cvt_x = cvt.xNode
-   cvt_y = cvt.yNode
-   print "N nodes", len(cvt_x)
-   cvt.set_pixel_grid((0, img.shape[1]),
-                      (0, img.shape[0]))
-   segimage = cvt.make_segmap()
+   cvt = CVTessellation.from_image(pix_sn ** 2., generator_centroids)
+   cvt_xy = cvt.nodes
+   segimage = cvt.segmap
 
-Plotting this segmentation image and teh cooresponding Voronoi nodes, we see
+Plotting this segmentation image and the corresponding Voronoi nodes, we see
 
 .. image:: images/iso_sn_voronoi.*
 
--- scripts/demo_iso_sn_voronoi.py
+++ scripts/demo_iso_sn_voronoi.py
@@ -16,21 +16,32 @@ from tess.cvtessellation import CVTessellation
 
 
 def main():
+    # Prepare mock data set with an image of a 2D Gaussian and constant noise
     shape = (256, 256)
     pix_x, pix_y = np.meshgrid(np.arange(shape[1], dtype=float),
                                np.arange(shape[0], dtype=float))
-    # img = 5. * np.ones(shape, dtype=float)
-    # noise = np.ones(shape, dtype=float)
     img = 10. * np.exp(-((pix_x - 128.) ** 2. / (2. * 50. ** 2.) +
                          (pix_y - 128.) ** 2. / (2. * 50. ** 2.)))
-    # noise = 50. * np.sqrt(img)
     noise = np.ones(shape, dtype=float)
+    pix_sn = img / noise
+
+    # Accrete pixels into equal S/N bins to make a set of Voronoi generators
     accretor = EqualSNAccretor(img, noise, 50., start=(0, 0))
     accretor.cleanup()
-    print "accretor.bin_sn", accretor.bin_sn
     generator_centroids = accretor.centroids
-    print "N centroids", generator_centroids.shape[0]
 
+    plot_segmap(accretor.segimage, generator_centroids, "iso_sn_accretion")
+
+    # Make a centroidal Voronoi tessellation so that each cell has
+    # approximately equal S/N
+    cvt = CVTessellation.from_image(pix_sn ** 2., generator_centroids)
+    cvt_xy = cvt.nodes
+    segimage = cvt.segmap
+
+    plot_segmap(segimage, cvt_xy, "iso_sn_voronoi")
+
+
+def plot_segmap(segmap, nodes, plot_path):
     fig = Figure(figsize=(3.5, 3.5), frameon=False)
     canvas = FigureCanvas(fig)
     gs = gridspec.GridSpec(1, 1,
@@ -38,37 +49,8 @@ def main():
                            wspace=None, hspace=None,
                            width_ratios=None, height_ratios=None)
     ax = fig.add_subplot(gs[0])
-    ax.imshow(accretor.segimage, interpolation='None', aspect='equal')
-    ax.scatter(generator_centroids[:, 0], generator_centroids[:, 1],
-               c='k', marker='o', s=4)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_ylim(0, 255)
-    ax.set_xlim(0, 255)
-    gs.tight_layout(fig, pad=1.08, h_pad=None, w_pad=None, rect=None)
-    canvas.print_figure("iso_sn_accretion.png", format="png")
-
-    pix_sn = img / noise
-    cvt = CVTessellation(pix_x.flatten(),
-                         pix_y.flatten(),
-                         pix_sn.flatten() ** 2.,
-                         node_xy=generator_centroids)
-    cvt_x = cvt.xNode
-    cvt_y = cvt.yNode
-    print "N nodes", len(cvt_x)
-    cvt.set_pixel_grid((0, img.shape[1]),
-                       (0, img.shape[0]))
-    segimage = cvt.make_segmap()
-
-    fig = Figure(figsize=(3.5, 3.5), frameon=False)
-    canvas = FigureCanvas(fig)
-    gs = gridspec.GridSpec(1, 1,
-                           left=0.15, right=0.95, bottom=0.15, top=0.95,
-                           wspace=None, hspace=None,
-                           width_ratios=None, height_ratios=None)
-    ax = fig.add_subplot(gs[0])
-    ax.imshow(segimage, interpolation='None', aspect='equal')
-    ax.scatter(cvt_x, cvt_y, c='k', marker='o', s=4)
+    ax.imshow(segmap, interpolation='None', aspect='equal')
+    ax.scatter(nodes[:, 0], nodes[:, 1], c='k', marker='o', s=4)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_ylim(0, 255)
